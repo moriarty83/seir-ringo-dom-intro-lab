c22735086a0903499ff3e2463744829e30e889b6
moriarty83 <moriarty83@gmail.com>
2021-08-11 12:50:10 -0400

    ignore vscode

--- src/main.ts
+++ src/main.ts
@@ -25,8 +25,7 @@ mainEl.innerHTML = "<h1>SEI Rocks!</h1>"
 
 // Task 1.3
 // Add a class of flex-ctrto mainEl.
-
-mainEl.className = "flex-ctr";
+mainEl.classList.add("flex-ctr");
 
 // Task 2.0
 // Select and cache the <nav id="top-menu">element in a variable named topMenuEl.
